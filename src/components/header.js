0c084680038d60ead9ece1501ad75fabce9555df
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-01-26 16:53:59 +0530

    added header component

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// header with a logo only
+import React from 'react';
+import logo from '../images/logo.png';
+
+const Header = () => (
+    <header>
+            <img src={logo} alt="logo" />
+    </header>
+);
+
+export default Header;
