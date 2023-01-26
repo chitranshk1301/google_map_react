143d12b3b01276e6d73e6800e19a78423d45aee3
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-01-26 18:39:31 +0530

    added more stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './components/Header';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='container'>
+        <h1 className='subtitle'>Let's calculate <strong>distance</strong> from Google maps</h1>
+        <Form />
+      </div>
     </div>
   );
 }
